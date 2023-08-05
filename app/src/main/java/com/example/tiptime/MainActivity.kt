062a6030eb238dd87d7b2845df4799b483371e48
Prateek <prateeksinha510@gmail.com>
2023-08-05 20:54:36 +0530

    changes done

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -180,11 +180,7 @@ fun RoundTheTipRow(modifier: Modifier = Modifier,
 
 
 
-/**
- * Calculates the tip based on the user input and format the tip amount
- * according to the local currency.
- * Example would be "$10.00".
- */
+
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0 ,
                          roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
